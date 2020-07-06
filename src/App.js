d165d5b47d8e63cce8b860389354dde8e8d16e2e
Uno <ltung630@gmail.com>
2020-07-06 15:30:33 +0700

    - Init code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App () {
+	return (
+      <div className="App">
+        <h1 className="title">To Do List</h1>
+      </div>
+    )
+}
+
+export default App;
