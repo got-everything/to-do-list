907525ed643f97946a67558142ecedf36bf52183
Uno22 <ltung630@gmail.com>
2020-07-07 23:59:27 +0700

    - Event basic: onKeyUp

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import ToDoItem from './ToDoItem';
 
 class App extends Component {
 
+  constructor () {
+    super();
+
+    this.onKeyUp = this.onKeyUp.bind(this);
+  }
+
+  onKeyUp (event) {
+    console.log(event.keyCode);
+  }
+
   render () {
     return (
       <div>
         <h1>To Do List</h1>
+        {/* <input type="text" placeholder="add new todo" onKeyUp={this.onKeyUp}></input> */}
         <ul>
           <ToDoItem value="Get up early"/>
         </ul>
